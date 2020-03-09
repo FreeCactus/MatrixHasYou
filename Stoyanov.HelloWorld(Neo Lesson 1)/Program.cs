8fdfd35d63eefcbb63b820d8bfaf96302bec6c4e
FreeCactus <61978341+FreeCactus@users.noreply.github.com>
2020-03-09 22:48:05 +0800

    Add files via upload

---
+++ Stoyanov.HelloWorld(Neo Lesson 1)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stoyanov.Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+            
+
+
+        {   
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadLine();
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadLine();
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit."); 
+            Console.ReadLine();
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadLine();
+            System.Threading.Thread.Sleep(4000);
+            
+
+
+        }
+    }
+}
